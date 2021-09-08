589b77096e54864290ec08b101320f472ff07db3
Kneyder <Kevinneyderbutronm@gmail.com>
2021-09-08 17:25:30 -0400

    List view funcionando

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/src/providers/menu_provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({ Key? key }) : super(key: key);
@@ -14,21 +15,42 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
-    return ListView(
-      children: _listaItems(),
+
+    // print(menuProvider.options);
+    // menuProvider.cargarData().then((value) {
+    //   print('$value -------');
+    // });
+
+    // return ListView(
+    //   children: _listaItems(),
+    // );
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        print('builder');
+        print(snapshot.data);
+        return ListView(
+          children: _listaItems( snapshot.data!),
+        );
+      }, 
     );
   }
 
-  List<Widget> _listaItems() {
-    return [
-      ListTile( title: Text('Hello'),),
-      Divider(),
-      ListTile( title: Text('Hello'),),
-      Divider(),
-      ListTile( title: Text('Hello'),),
-      Divider(),
-      ListTile( title: Text('Hello'),),
+  List<Widget> _listaItems( List<dynamic> data) {
+    final List<Widget> options = [];
+    data.forEach((element) {
+      final widgetTemp = ListTile(
+        title: Text(element['texto']),
+        leading: Icon(Icons.contact_mail_rounded),
+        trailing: Icon(Icons.arrow_forward_ios),
+        onTap: (){
 
-    ];
+        },
+      );
+      options..add(widgetTemp)
+             ..add(Divider());
+    });
+    return options;
   }
 }
---
+++ lib/src/providers/menu_provider.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart' show rootBundle;
+
+class _MenuProvider{
+
+  List<dynamic> options = [];
+
+  _MenuProvider(){
+    cargarData();
+  }
+
+  Future<List<dynamic>> cargarData() async{
+    final res = await rootBundle.loadString('data/menu_opts.json');
+              
+    Map dataMap = jsonDecode(res);
+    // print(dataMap['rutas']);
+    options = dataMap['rutas'];
+
+    return options;
+  }
+
+  
+}
+
+final menuProvider = new _MenuProvider();
